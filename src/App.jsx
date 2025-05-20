ae436b2ee5ca6a97b49fb662f96adcb3cde9ac12
Rishav <rishavraj4273@gmail.com>
2025-05-20 14:43:01 +0530

    fallback url

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Trek from './pages/Trek';
+import TrekDetails from './pages/TrekDetails';
+import NotFound from './pages/NotFound';
+import Data from "../data.json";
+import useTrekStore from './store/trekStore';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const sampleTrek = Data.treks;
+  const setTreks = useTrekStore((state) => state.setTreks);
+  setTreks(sampleTrek);
 
   return (
-    <>
+    <Router>
+      <ScrollToTop/>
+      <main className='mt-17'>
       <Navbar/>
-      <div className='mx-24'>
-        <Home/>
-      </div>
-    </>
+        <Routes>
+        <Route path="*" element={<NotFound/>} />
+          <Route path="/" element={<Home/>}/>
+          <Route path="/treks" element={<Trek/>}/>
+          <Route path="/trek-details/:slug" element={<TrekDetails/>}/>
+        </Routes>
+      </main>
+      <Footer/>
+    </Router>
   )
 }
 
